255d62945c4a14cb53fdfc68d7b0d989b35f83b5
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-05 13:34:45 -0300

    Implementa ordenação de contatos

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -21,6 +21,75 @@ class Filters extends React.Component {
     setContacts(filteredContacts)
   }
 
+  orderByName = () => {
+    const { allContacts, setContacts } = this.props
+    const contacts = allContacts.sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1
+      }
+      if (a.name > b.name) {
+        return 1
+      }
+      return 0
+    })
+
+    setContacts(contacts)
+  }
+
+  orderByCountry = () => {
+    const { allContacts, setContacts } = this.props
+    const contacts = allContacts.sort(function (a, b) {
+      if (a.country < b.country) {
+        return -1
+      }
+      if (a.country > b.country) {
+        return 1
+      }
+      return 0
+    })
+
+    setContacts(contacts)
+  }
+
+  orderByCompany = () => {
+    const { allContacts, setContacts } = this.props
+    const contacts = allContacts.sort(function (a, b) {
+      if (a.company < b.company) {
+        return -1
+      }
+      if (a.company > b.company) {
+        return 1
+      }
+      return 0
+    })
+
+    setContacts(contacts)
+  }
+
+  orderByDepartment = () => {
+    const { allContacts, setContacts } = this.props
+    const contacts = allContacts.sort(function (a, b) {
+      if (a.department < b.department) {
+        return -1
+      }
+      if (a.department > b.department) {
+        return 1
+      }
+      return 0
+    })
+
+    setContacts(contacts)
+  }
+
+  orderByAdmissionDate = () => {
+    const { allContacts, setContacts } = this.props
+    const contacts = allContacts.sort(function (a, b) {
+      return new Date(b.admissionDate) - new Date(a.admissionDate)
+    })
+
+    setContacts(contacts)
+  }
+
   render() {
     return (
       <div className="container">
@@ -39,23 +108,26 @@ class Filters extends React.Component {
             </button>
           </div>
 
-          <button className="filters__item is-selected">
+          <button
+            className="filters__item is-selected"
+            onClick={this.orderByName}
+          >
             Nome <i className="fas fa-sort-down" />
           </button>
 
-          <button className="filters__item">
+          <button className="filters__item" onClick={this.orderByCountry}>
             País <i className="fas fa-sort-down" />
           </button>
 
-          <button className="filters__item">
+          <button className="filters__item" onClick={this.orderByCompany}>
             Empresa <i className="fas fa-sort-down" />
           </button>
 
-          <button className="filters__item">
+          <button className="filters__item" onClick={this.orderByDepartment}>
             Departamento <i className="fas fa-sort-down" />
           </button>
 
-          <button className="filters__item">
+          <button className="filters__item" onClick={this.orderByAdmissionDate}>
             Data de admissão <i className="fas fa-sort-down" />
           </button>
         </section>
